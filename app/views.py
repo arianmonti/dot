3a69c71eed57ac4b9a7276879d2ea5eefa898394
arianmonti <a.m.ontazeri1385@gmail.com>
2020-11-22 11:03:24 +0330

    add register view

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, logout as logout_user, login as login_user
 from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
 
 from .models import Post
 
@@ -30,3 +31,23 @@ def login(request):
 def logout(request):
     logout_user(request)
     return redirect('/')
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+        user = User.objects.filter(username=username).first()
+        user_email = User.objects.filter(username=username).first()
+        if user is not None or user_email is not None:
+            messages.error(request, 'user exists.')
+        if password != confirm_password:
+            messages.error(request, 'passwords are not same.')
+        new_user = User.objects.create_user(username=username, email=email, password=password)
+        new_user.save()
+        return redirect('/login/')
+    return render(request, 'register.html')
